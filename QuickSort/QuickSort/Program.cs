d949643824e8d9b82840014b47272900a45028ab
wheel0012 <39899790+wheel0012@users.noreply.github.com>
2019-05-21 13:07:47 +0900

    QuickSort Arrived
    init

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static Program instance;
+        static void Main(string[] args)
+        {
+            instance = new Program();
+            //Console.WriteLine("Hello World!");
+            List<int> arr = new List<int>() { 1, 5, 2, 9, 0, 6, 2, 61, 4, 0, 2};
+            arr = instance.Sort(arr, arr[arr.Count / 2]);
+            foreach(int i in arr)
+            Console.WriteLine(i);
+        }
+        List<int> Sort(List<int> arr, int pivot)
+        {
+            Console.WriteLine(pivot);
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            List<int> middle = new List<int>();
+            foreach (int i in arr)
+            {
+                if (i > pivot) right.Add(i);
+                else if (i < pivot) left.Add(i);
+                else middle.Add(i);
+            }
+            arr.Clear();
+            foreach (int j in left) Console.Write(j);
+            Console.WriteLine();
+            foreach (int j in right) Console.Write(j);
+            Console.WriteLine();
+            if (left.Count > 1) left = Sort(left, left[left.Count / 2]);
+            if(right.Count >1) right  = Sort(right, right[right.Count / 2]);
+            arr.AddRange(left);
+            arr.AddRange(middle);
+            arr.AddRange(right);
+            return arr;
+        }
+    }
+}
