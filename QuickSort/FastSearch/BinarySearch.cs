f129b3e0ede536380ab80fc7abd92e2955a601aa
wheel0012 <wheel0012@outlook.kr>
2019-05-31 14:37:01 +0900

    trash

--- QuickSort/FastSearch/BinarySearch.cs
+++ QuickSort/FastSearch/BinarySearch.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Dynamic;
 namespace FastSearch
 {
     public class BinarySearch
     {
+        public static T Do<T>(List<T> arr, T value)
+        {
+            if(comparebe arr.ElementAt(arr.Count) > value);
+            return Value;
+        }
+        private int CompareTo<T>(T value1, T value2)
+        {
+
+            return 0;
+        }
     }
 }
